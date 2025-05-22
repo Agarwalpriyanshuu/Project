55c810db2e3596718208a6ed2dc4371dd09d3895
PriyanshuAgarwal <165584262+Agarwalpriyanshuu@users.noreply.github.com>
2025-05-22 11:53:51 +0530

    Create git.sh

---
+++ git.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install git -y
+mkdir project | cd project
+touch index.html
+echo 'FROM nginx\COPY * /usr/share/nginx/html/'
+git init
+git config --global user.name "user_name"
+git config --global user.email "email_id"
+git add .
+ssh-keygen -t rsa
+cat ~/.ssh/id_rsa.pub
